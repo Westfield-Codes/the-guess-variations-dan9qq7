ee1f4a52ba1b2feb581b7a16f7c1f8944785e900
dan9qq7 <75699450+dan9qq7@users.noreply.github.com>
2025-03-10 14:54:31 +0000

    Done Quitter

--- pseudocode/3.guess-loop-quit.js
+++ pseudocode/3.guess-loop-quit.js
@@ -1,27 +1,29 @@
 /* Simple Guess Program */
 
 // define function guessGame
-
+function guessgame(){
   // generate a random integer 1-100
+let answer = Math.floor(Math.random()*100+1);
 
   // display the integer for testing purposes only
-
+alert(answer)
   // create guess and set equal to 0 
-
+let guess = 0
   // loop while guess is not equal to answer or to "q"
-
+while (guess != answer && guess != "q") {
     // set guess equal to asking user to "Guess a number, 1-100"
-
-    // if guess equals "q" display "Quitter!"
-
+ guess = prompt("Guess a number 1-100 or q to quit.")
+    // if guess equals "q" display ""
+if (guess == "q") alert("Quitter!");
     // or if guess equals answer display "Correct!" 
-
+else if (guess == answer) alert("Correct!");
     // or if guess is greater than answer display "Too high!" 
-
+else if (guess > answer) alert("too high");
     // or if guess is less than answer display "Too low!" 
-
+else if (guess < answer) alert("too Low");
     // or say "Bad input"
-
+else alert("Bad input.");
   // end the loop
-
+}
 // end the function
+}
