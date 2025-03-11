6f217a521e546f7d48075bd7f5271ceb22007ac2
dan9qq7 <75699450+dan9qq7@users.noreply.github.com>
2025-03-11 14:55:14 +0000

    almost done 5

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-/* Guess Game Program by [your name here]
- * Adds the ability to play again. 
- */
-
-/* newGame is an outer loop giving the user an option 
- * to play again, using a boolean variable called again.
- */
+/* Guess Game Program */
 
 // define function newGame
 function newGame(){
   // create a Boolean variable for again, set to true
-var again = true
+let again = true
+  // run the guessGame function
+guessGame()
   // loop as long as again is true 
 while (again == true){
-    // run the guessGame function 
-guessGame()
     // ask to play again with again set to a confirm
 again = confirm("play again?")
+    // if again is true run the guessGame function
+if (again = true) {
+  guessGame()
+}
     // close the loop
 }
   // thank the player for playing
-alert("i am not greatful for you playing this.")
+alert("i am not grateful for your participation.")
   // close the function
 }
 
-  
-// create a function called guessGame 
+// create a function called guessGame
 function guessGame(){
   // create guess and set equal to 0 
-var guess = 0
+let guess = 0
   // create tries and set equal to 0 
 let tries = 0
   // create variable answer and generate a random integer 1-100
-var answer = Math.floor(Math.random()*100+1);
+var answer = math.floor(math.random()*100+1)
   // display the integer for testing purposes only
 alert(answer)
   // loop while guess is not equal to answer
@@ -39,15 +36,17 @@ while (guess != answer){
     // set guess equal to asking user to "Guess a number, 1-100"
 guess = prompt("Guess a number, 1-100")
     // add one to tries
-tries = (tries + 1)
+tries = tries ++
     // if guess equals answer display "Correct in [tries] tries!" 
-if (guess == answer) alert("correct in " + tries + " tries!")
+if (guess == answer) alert("correct!")
+    // or if guess is "q" break the loop
+
     // or if guess is greater than answer display "Too high!" 
-else if (guess > answer) alert("Too High!")
+
     // or if guess is less than answer display "Too low!" 
-else if (guess < answer) alert("Too low!")
+
     // if it's none of those, say "Bad Input!"
-else alert("bad input!")
+
     // end the loop
 }
   // end the function
